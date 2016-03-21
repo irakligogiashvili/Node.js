a8fb0ad98016a328ef550c7242e087d6288faeeb
irakli <i.gogiashvili@mia.gov.ge>
2016-03-21 17:11:22 +0400

    async

---
+++ async_example/server.js
@@ -0,0 +1,57 @@
+var fs = require('fs')
+    , completedTasks = 0
+    , tasks = []
+    , wordCounts = {}
+    , filesDir = './async_example/texts';
+
+
+function checkIfComplete() {
+    completedTasks++;
+
+    if (completedTasks == tasks.length) {
+        logWordCount();
+    }
+}
+
+function logWordCount() {
+    for (var index in wordCounts) {
+        console.log(index + ': ' + wordCounts[index]);
+    }
+}
+
+function countWordsInText(text) {
+    var words = text.toString().toLowerCase().split(/\W+/).sort();
+
+    for (var index in words) {
+        var word = words[index];
+
+        if (word) {
+            wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
+        }
+    }
+}
+
+fs.readdir(filesDir, function (err, files) {
+    if (err) throw  err;
+
+    for (var index in files) {
+        var task = (function (file) {
+
+            return function () {
+                fs.readFile(file, function (err, text) {
+                    if (err) throw  err;
+
+                    countWordsInText(text);
+                    checkIfComplete();
+                })
+            };
+
+        })(filesDir + '/' + files[index]);
+
+        tasks.push(task);
+    }
+
+    for (var task in tasks) {
+        tasks[task]();
+    }
+});

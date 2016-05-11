9915f4785b9a19367d1e69d6c76d71cb92f7ff63
irakli <i.gogiashvili@mia.gov.ge>
2016-05-11 13:23:51 +0400

    remove cfg

--- oracl/dbconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: process.env.NODE_ORACLEDB_USER || "soso",
-    password: process.env.NODE_ORACLEDB_PASSWORD || "Bl00mb3rg",
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "orascan.mia.ge:1521/orascan.mia.ge",
-    externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-};

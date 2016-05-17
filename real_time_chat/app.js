c0f84ede4e0569d68c7294cc11a1213c572cf435
irakli <i.gogiashvili@mia.gov.ge>
2016-05-17 12:12:07 +0400

    cool

--- real_time_chat/app.js
+++ real_time_chat/app.js
@@ -5,13 +5,27 @@ var http = require('http')
 var ROOT = __dirname;
 ROOT = path.join(ROOT, 'public');
 
+var urlMap = {
+    '/': function (request, response) {
+        response.end('/');
+    },
+    '/map': function (request, response) {
+        response.end('map');
+    }
+};
+
 var server = http.createServer(function (request, response) {
 
     if (request.url.indexOf('/public') != -1) {
         staticFile(request, response, ROOT);
-    }
+    } else if (urlMap[request.url]) {
 
-    console.log('createServer');
+        urlMap[request.url](request, response);
+
+    } else {
+        response.statusCode = 404;
+        response.end('Not Found');
+    }
 });
 
 server.listen(3000);
